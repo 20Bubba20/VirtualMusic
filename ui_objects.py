85dc62196af4bc0856e47a0942564dab50a0d11f
WhoDis-MaName <andrawilliams03.cs@gmail.com>
2024-11-20 10:52:39 -0600

    Optimize UI API

--- ui_objects.py
+++ ui_objects.py
@@ -2,27 +2,152 @@ import cv2 as cv
 from math import sin, cos, sqrt, pi
 
 class UIShape:
-    def __init__(self, type: str, color: tuple[int, int, int], dx_start: int, dy_start: int, dx_end: int, dy_end: int, thickness: int, angle: float = None, start_angle: float = None, end_angle: float = None):
-        self.dx_start = dx_start
-        self.dy_start = dy_start
-        self.dx_end = dx_end
-        self.dy_end = dy_end
-        self.border = thickness
-        self.color = color
+    def __init__(self, type: str, color: tuple[int, int, int], dx: int, dy: int, thickness: int):
+        self.dx = dx
+        self.dy = dy
         self.type = type
-        if type == 'ellipse':
-            if start_angle is None or end_angle is None:
-                start_angle = 0
-                end_angle = 360
-            if angle is None:
-                angle = 0
+        self.color = color
+        self.thickness = thickness
+        self.range()
+        
+    def draw(self, img, parent_pos):
+        match self.type:
+                case 'line':
+                    cv.line(img, (parent_pos[0] + self.dx_start, parent_pos[0] + self.dy_start), (parent_pos[0] + self.dx_end, parent_pos[0] + self.dy_end), self.color, self.border)
+                case 'rectangle':
+                    cv.rectangle(img, (parent_pos[0] + self.dx_start, parent_pos[0] + self.dy_start), (parent_pos[0] + self.dx_end, parent_pos[0] + self.dy_end), self.color, self.border)
+                case 'ellipse':
+                    center = (int((self.dx_end + self.dx_start)/2) + parent_pos[0], int((self.dy_end + self.dy_start)/2) + parent_pos[0])
+                    axis = (
+                        abs(self.dx_end - self.dx_start),
+                        abs(self.dy_end - self.dy_start)
+                    )
+                    cv.ellipse(img, center, axis, self.angle, self.start_angle, self.end_angle, self.color, thickness=self.border)
+                case 'text':
+                    cv.putText(img, self.text, (parent_pos[0] + self.dx, parent_pos[0] + self.dy), self.font, self.font_size, self.color, self.thickness, cv.LINE_AA)
+    def range(self):
+        x_max = -10000
+        x_min = 10000
+        y_max = -10000
+        y_min = 10000
+        if self.type == 'text':
+            if self.dx < x_min:
+                x_min = self.dx
+            if self.dy < y_min:
+                y_min = self.dy
+
+        if self.type == 'elipse':
+            center = (self.x + self.dx_start, self.y + self.dy_start)
+            r1 = self.dx_end
+            r2 = self.dy_end
+            angle = self.angle
+
+            ux = r1 * cos(angle)
+            uy = r1 * sin(angle)
+            vx = r2 * cos(angle + pi/2)
+            vy = r2 * sin(angle + pi/2)
+
+            bbox_halfwidth = sqrt(ux*ux + vx*vx)
+            bbox_halfheight = sqrt(uy*uy + vy*vy)
+
+            
+            minx= center.x - bbox_halfwidth
+            miny= center.y - bbox_halfheight
+            maxx= center.x + bbox_halfwidth 
+            maxy= center.y + bbox_halfheight
+            if minx < x_min:
+                x_min = minx
+            if miny < y_min:
+                y_min = miny
+            if maxx > x_max:
+                x_min = maxx
+            if maxy > y_max:
+                y_min = maxy
+
+        if self.dx_start < x_min:
+            x_min = self.dx_start
+        if self.dy_start < y_min:
+            y_min = self.dy_start
+        if self.dx_start > x_max:
+            x_max = self.dx_start
+        if self.dy_start > y_max:
+            y_max = self.dy_start
+            
+        if self.dx_end < x_min:
+            x_min = self.dx_end
+        if self.dy_end < y_min:
+            y_min = self.dy_end
+        if self.dx_end > x_max:
+            x_max = self.dx_end
+        if self.dy_end > y_max:
+            y_max = self.dy_end
+        pass
+        
+class UIEllipse(UIShape):
+    def __init__(self, name: str, color: tuple[int, int, int], dx: int, dy: int, dx_size: int, dy_size: int, thickness: int, angle: float = 0, start_angle: float = 0, end_angle: float = 360):
+        self.name = name
+        self.dx_size = dx_size
+        self.dy_size = dy_size
         self.angle = angle
         self.start_angle = start_angle
         self.end_angle = end_angle
+        super().__init__('ellipse', color, dx, dy, thickness)
+        
+    def draw(self,img, parent_pos):
+        center = (int((self.dx_size + self.dx)/2) + parent_pos[0], int((self.dy_end + self.dy_start)/2) + parent_pos[0])
+        axis = (
+            abs(self.dx_size - self.dx),
+            abs(self.dy_size - self.dy)
+        )
+        cv.ellipse(img, center, axis, self.angle, self.start_angle, self.end_angle, self.color, thickness=self.border)
+        
+    def range(self):
+        center = (self.x + self.dx_start, self.y + self.dy_start)
+        r1 = self.dx_end
+        r2 = self.dy_end
+        angle = self.angle
+
+        ux = r1 * cos(angle)
+        uy = r1 * sin(angle)
+        vx = r2 * cos(angle + pi/2)
+        vy = r2 * sin(angle + pi/2)
+
+        bbox_halfwidth = sqrt(ux*ux + vx*vx)
+        bbox_halfheight = sqrt(uy*uy + vy*vy)
+
+        
+        self.min_x= center.x - bbox_halfwidth
+        self.min_y= center.y - bbox_halfheight
+        self.max_x= center.x + bbox_halfwidth 
+        self.max_y= center.y + bbox_halfheight
+
+class UIRect(UIShape):
+    def __init__(self, color: tuple[int, int, int], dx_start: int, dy_start: int, dx_end: int, dy_end: int, thickness: int):
+        self.dx_end = dx_end
+        self.dy_end = dy_end
+        super().__init__('rectangle', color, dx_start, dy_start, thickness)
+        
+    def draw(self, img, parent_pos):
+        cv.rectangle(img, (parent_pos[0] + self.dx_start, parent_pos[0] + self.dy_start), (parent_pos[0] + self.dx_end, parent_pos[0] + self.dy_end), self.color, self.border)
+        
+    def range(self):
+        self.min_x= min(self.dx,self.dx_end)
+        self.min_y= min(self.dy,self.dy_end)
+        self.max_x= max(self.dx,self.dx_end)
+        self.max_y= max(self.dy,self.dy_end)
+        
+class UILine(UIRect):
+    def __init__(self, color, dx_start, dy_start, dx_end, dy_end, thickness):
+        super().__init__(color, dx_start, dy_start, dx_end, dy_end, thickness)
+        self.type = 'line'
+        
+    def draw(self, img, parent_pos):
+        cv.line(img, (parent_pos[0] + self.dx, parent_pos[0] + self.dy), (parent_pos[0] + self.dx_end, parent_pos[0] + self.dy_end), self.color, self.thickness)
+                
         
 
-class UIText:
-    def __init__(self, dx, dy, text, scale, color, thickness):
+class UIText(UIShape):
+    def __init__(self, color, dx, dy, text, scale, thickness):
         self.dx = dx
         self.dy = dy
         self.text = text
@@ -34,7 +159,7 @@ class UIText:
     pass 
 
 class UI_Element:
-    def __init__(self, x: int, y: int, draw_list: list[UIShape | UIText]):
+    def __init__(self, x: int, y: int, draw_list: list[UIShape]):
         self.x: int = x
         self.y: int = y
         self.draw_list = draw_list
@@ -42,65 +167,10 @@ class UI_Element:
         self.set_font()
     
     def range(self):
-        x_max = -10000
-        x_min = 10000
-        y_max = -10000
-        y_min = 10000
-        for shape in self.draw_list:
-            if shape.type == 'text':
-                if shape.dx < x_min:
-                    x_min = shape.dx
-                if shape.dy < y_min:
-                    y_min = shape.dy
-                continue
-            
-            if shape.type == 'elipse':
-                center = (self.x + shape.dx_start, self.y + shape.dy_start)
-                r1 = shape.dx_end
-                r2 = shape.dy_end
-                angle = shape.angle
-
-                ux = r1 * cos(angle)
-                uy = r1 * sin(angle)
-                vx = r2 * cos(angle + pi/2)
-                vy = r2 * sin(angle + pi/2)
-
-                bbox_halfwidth = sqrt(ux*ux + vx*vx)
-                bbox_halfheight = sqrt(uy*uy + vy*vy)
-
-                
-                minx= center.x - bbox_halfwidth
-                miny= center.y - bbox_halfheight
-                maxx= center.x + bbox_halfwidth 
-                maxy= center.y + bbox_halfheight
-                if minx < x_min:
-                    x_min = minx
-                if miny < y_min:
-                    y_min = miny
-                if maxx > x_max:
-                    x_min = maxx
-                if maxy > y_max:
-                    y_min = maxy
-                continue
-            
-            if shape.dx_start < x_min:
-                x_min = shape.dx_start
-            if shape.dy_start < y_min:
-                y_min = shape.dy_start
-            if shape.dx_start > x_max:
-                x_max = shape.dx_start
-            if shape.dy_start > y_max:
-                y_max = shape.dy_start
-                
-            if shape.dx_end < x_min:
-                x_min = shape.dx_end
-            if shape.dy_end < y_min:
-                y_min = shape.dy_end
-            if shape.dx_end > x_max:
-                x_max = shape.dx_end
-            if shape.dy_end > y_max:
-                y_max = shape.dy_end
-            pass
+        x_max = max([shape.x_max for shape in self.draw_list])
+        x_min = min([shape.x_min for shape in self.draw_list])
+        y_max = max([shape.y_max for shape in self.draw_list])
+        y_min = min([shape.y_min for shape in self.draw_list])
         
         self.x_max = x_max + self.x
         self.x_min = x_min + self.x
@@ -113,21 +183,7 @@ class UI_Element:
     
     def draw(self, img):
         for shape in self.draw_list:
-            match shape.type:
-                case 'line':
-                    cv.line(img, (self.x + shape.dx_start, self.y + shape.dy_start), (self.x + shape.dx_end, self.y + shape.dy_end), shape.color, shape.border)
-                case 'rectangle':
-                    cv.rectangle(img, (self.x + shape.dx_start, self.y + shape.dy_start), (self.x + shape.dx_end, self.y + shape.dy_end), shape.color, shape.border)
-                case 'ellipse':
-                    center = (int((shape.dx_end + shape.dx_start)/2) + self.x, int((shape.dy_end + shape.dy_start)/2) + self.y)
-                    axis = (
-                        abs(shape.dx_end - shape.dx_start),
-                        abs(shape.dy_end - shape.dy_start)
-                    )
-                    cv.ellipse(img, center, axis, shape.angle, shape.start_angle, shape.end_angle, shape.color, thickness=shape.border)
-                case 'text':
-                    cv.putText(img, shape.text, (self.x + shape.dx, self.y + shape.dy), self.font, shape.font_size, shape.color, shape.thickness, cv.LINE_AA)
-            pass
+            shape.draw(img,(self.x, self.y))
         pass
     
     def set_font(self, font = cv.FONT_HERSHEY_SIMPLEX):
--- ui_test.py
+++ ui_test.py
@@ -2,48 +2,45 @@ import numpy as np
 import cv2
 from ui_objects import *
 
-
-resolution = (500, 500)
-button = UI_Element(
-    x = 100,
-    y = 100,
+scale = (16, 9)
+k = 80
+resolution = (k*scale[0],k*scale[1])
+# print(resolution)
+PrimaryColor = (18, 96, 132)
+TitleBig = UI_Element(
+    name = 'title',
+    x = int((resolution[0]/2)),
+    y = 120,
     draw_list=[
-        UIShape('rectangle', (0,150,150), 0, 0, 100, 100, 5)
-        # ,
-        # UIText(10,0, 'This is Text', 2, (255, 255, 255), 4)
+        UIText((0, 0, 0), 0, 0, 'Virtual Music', 2, 4),
+        UIEllipse((0,0,0), 0,0, 3, 3, -1)
     ]
 )
 
-center = UI_Element(
-    x = int(resolution[0]/2),
-    y = int(resolution[1]/2),
-    draw_list = [
-        UIShape('ellipse', (255,255,255), -10, -10, 10, 10, 3)
+PracticeButton = UI_Element(
+    name = 'practice',
+    x = int((resolution[0]/2))-50,
+    y = 155,
+    draw_list=[
+        UIRect(PrimaryColor, 0,0, 300, 20, -1),
+        UIRect((0,0,0), 0,0, 300, 20, -1),
+        UIText((255,255,255), 15, 15, 'Practice', 1, 2)
     ]
 )
 
-print(center.x)
-print(center.y)
-dx, dy = (10, 15)
+
+HomeScene = [
+    TitleBig
+]
+
+
 while True:
-    # Create a black image
-    img = np.full((resolution[0],resolution[1],3), 255, np.uint8)
-    button.draw(img)
-    center.draw(img)
+    # Create a white image
+    img = np.full((resolution[1],resolution[0],3), 255, np.uint8)
+    for element in HomeScene:
+        element.draw(img)
     cv2.imshow("Test UI", img)
-    # print(f'{button.size()}')
-    # print(f'x: {button.x}; y: {button.y}')
-    # print(f'dx: {dx}; dy: {dy}')
-    # print(f'x_max: {button.x_max}; y_max: {button.y_max}')
-    # print(f'x_min: {button.x_min}; y_min: {button.y_min}')
     
-    button.change_position(button.x + dx, button.y + dy)
-    if button.x_max >= resolution[0] or button.x_min <= 0:
-        dx = dx * -1
-    if button.y_max >= resolution[1] or button.y_min <= 0:
-        dy = dy * -1
-    if button.collision(center.x,center.y):
-        print("In Center")
     if cv2.waitKey(1) == ord('q') or cv2.waitKey(1) == 27:
         print('Exiting')
         cv2.destroyAllWindows()
